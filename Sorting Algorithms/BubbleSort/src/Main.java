568fdfb2864f21101cd0f4558095a9ffaefb442d
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-06-08 13:13:01 -0400

    Update BubbleSort

--- Sorting Algorithms/BubbleSort/src/Main.java
+++ Sorting Algorithms/BubbleSort/src/Main.java
@@ -1,9 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int [] intArray ={20,35,-15,7,55,1,-22};
 
-        // our assumption is in the beginning the array is unsorted
-        // So we will be initializing the last unsortedIndex by the length of the array-1
+        // our assumption is in the beginning the array is unsorted so
+        // we will be initializing the last unsortedIndex by the length of the array
 
         for(int lastUnsortedIndex = intArray.length-1;lastUnsortedIndex>0;lastUnsortedIndex--){
             /*
@@ -31,13 +33,13 @@ public class Main {
         public static void swap(int [] array, int i,int j) {
             if(i==j){
                 return;
-                //Swapping is not required for these elements
+                //No element is actually there to swap
             }
             int temp= array[i];
             array[i]=array[j];
             array[j]=temp;
 
-             //Swapping the elements with the help of a temp variable
+            //swapping here
         }
 
 
