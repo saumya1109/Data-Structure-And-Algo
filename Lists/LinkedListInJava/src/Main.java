221fb77309328eb3087f1a232aace7bbd01a9da7
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-06-14 16:55:31 -0400

    Update Main.java

--- Lists/LinkedListInJava/src/Main.java
+++ Lists/LinkedListInJava/src/Main.java
@@ -1,8 +1,7 @@
 import java.util.LinkedList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Employee janeJones = new Employee("Jane", "Jones", 123);
