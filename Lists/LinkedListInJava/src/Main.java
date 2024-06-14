9478acd003fe9406a06c850149194f4ffc55c2d6
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-06-14 16:55:07 -0400

    Adding Linked List JDK

---
+++ Lists/LinkedListInJava/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+
+        LinkedList<Employee> list = new LinkedList<>();
+        list.addFirst(janeJones);
+        list.addFirst(johnDoe);
+        list.addFirst(marySmith);
+        list.addFirst(mikeWilson);
+
+        list.addLast(billEnd);
+
+        list.removeFirst();
+        list.removeLast();
+
+        for(Employee employees: list) {
+            System.out.println(employees);
+        }
+
+
+    }
+}
